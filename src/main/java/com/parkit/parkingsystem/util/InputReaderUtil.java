21acd916d53be8977f5c7fd66974f989724cf32c
era Grandhomme <era.grandhomme@gmail.com>
2021-06-23 17:25:12 +0200

    correction class InputReaderUtil.java

--- src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
@@ -1,39 +1,21 @@
 package com.parkit.parkingsystem.util;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class InputReaderUtil {
+    private static Scanner scan;
+    private static final Logger logger = null;
 
-    private static Scanner scan = new Scanner(System.in);
-    private static final Logger logger = LogManager.getLogger("InputReaderUtil");
+    public InputReaderUtil() {
+        throw new Error("Unresolved compilation problems: \n\tThe import org.apache cannot be resolved\n\tThe import org.apache cannot be resolved\n\tLogger cannot be resolved to a type\n\tLogManager cannot be resolved\n\tLogger cannot be resolved to a type\n\tLogger cannot be resolved to a type\n");
+    }
 
     public int readSelection() {
-        try {
-            int input = Integer.parseInt(scan.nextLine());
-            return input;
-        }catch(Exception e){
-            logger.error("Error while reading user input from Shell", e);
-            System.out.println("Error reading input. Please enter valid number for proceeding further");
-            return -1;
-        }
+        throw new Error("Unresolved compilation problem: \n\tLogger cannot be resolved to a type\n");
     }
 
     public String readVehicleRegistrationNumber() throws Exception {
-        try {
-            String vehicleRegNumber= scan.nextLine();
-            if(vehicleRegNumber == null || vehicleRegNumber.trim().length()==0) {
-                throw new IllegalArgumentException("Invalid input provided");
-            }
-            return vehicleRegNumber;
-        }catch(Exception e){
-            logger.error("Error while reading user input from Shell", e);
-            System.out.println("Error reading input. Please enter a valid string for vehicle registration number");
-            throw e;
-        }
+        throw new Error("Unresolved compilation problem: \n\tLogger cannot be resolved to a type\n");
     }
-
-
 }
